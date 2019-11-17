1439266eb0254bc8db91771a288497347ac61db6
wasif <wasif0332@gmail.com>
2019-11-17 22:13:21 +0500

    create directory structure

---
+++ index.js
@@ -0,0 +1,25 @@
+const fp = require('fastify-plugin')
+const mongoose = require('mongoose');
+
+class FastifyMongoose {
+    constructor(config, next) {
+        
+    }
+}
+function FastifyMongoose() {
+    this.connection = null;
+    this.user = '';
+    this.password = '';
+    this.database = '';
+    this.hosts = [];
+    this.port = [];
+    this.defaultOptions = {};
+}
+
+FastifyMongoose.prototype.toUrl = function() {
+
+}
+
+module.exports = fp(FastifyMongoose, {
+    name: 'fastify-mongoose'
+});
