87e55aee90b62fa84d750850dec4ec0e1cda993c
wasif <wasif0332@gmail.com>
2019-12-15 12:01:04 +0500

    make Host file unit test

--- tests/unit/host/Host.test.js
+++ tests/unit/host/Host.test.js
@@ -4,12 +4,53 @@ import { stub } from 'sinon';
 
 describe('#Host', () => {
 
+    let instance = null;
+
+    beforeEach(() => {
+        instance = new Host('localhost', 27017);
+    });
+
+    afterEach(() => {
+        instance = null;
+    })
+
     describe('#constructor', () => {
 
         it('should set host and port', () => {
-            let instance = new Host('localhost', 27017);
             expect(instance.host).to.be.equal('localhost');
         });
+    });
 
+    describe('#set port', () => {
+        it('should set the port', () => {
+            instance.port = 22;
+            expect(instance.port).to.be.equal(22);
+        });
+    });
+
+    describe('#set host', () => {
+        it('should set the host', () => {
+            instance.host = 'anotherhost';
+            expect(instance.host).to.be.equal('anotherhost');
+        });
+    });
+
+    describe('#get port', () => {
+        it('should return the setted port', () => {
+            expect(instance.port).to.be.equal(27017);
+        });
+    });
+
+    describe('#get host', () => {
+        it('should set return the setted host', () => {
+            expect(instance.host).to.be.equal('localhost');
+        });
+    });
+
+    describe('#toString', () => {
+        it('should return full host string', () => {
+            expect(instance.toString()).to.be.a('string');
+            expect(instance.toString()).to.be.equal('localhost:27017');
+        });
     })
 })
