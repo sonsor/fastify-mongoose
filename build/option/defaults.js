9a4128802f144d34b52ea33228e3176ca671b6af
Wasif Farooq <wasif@192.168.10.11>
2020-05-04 14:05:09 +0500

    add test and commit code in master

--- build/option/defaults.js
+++ build/option/defaults.js
@@ -1,54 +1,10 @@
-"use strict";
-
-var cov_1n02ltwe9i = function () {
-  var path = "/home/wasif/projects/upstore/fastify-mongoose/src/option/defaults.js";
-  var hash = "b31e409f1d0826791ef653150d2cc44e77b78c0d";
-  var global = new Function("return this")();
-  var gcv = "__coverage__";
-  var coverageData = {
-    path: "/home/wasif/projects/upstore/fastify-mongoose/src/option/defaults.js",
-    statementMap: {},
-    fnMap: {},
-    branchMap: {},
-    s: {},
-    f: {},
-    b: {},
-    _coverageSchema: "43e27e138ebf9cfc5966b082cf9a028302ed4184",
-    hash: "b31e409f1d0826791ef653150d2cc44e77b78c0d"
-  };
-  var coverage = global[gcv] || (global[gcv] = {});
-
-  if (coverage[path] && coverage[path].hash === hash) {
-    return coverage[path];
-  }
-
-  return coverage[path] = coverageData;
-}();
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-var _default = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  autoIndex: false,
-  // Don't build indexes
-  reconnectTries: Number.MAX_VALUE,
-  // Never stop trying to reconnect
-  reconnectInterval: 500,
-  // Reconnect every 500ms
-  poolSize: 10,
-  // Maintain up to 10 socket connections
-  // If not connected, return errors immediately rather than waiting for reconnect
-  bufferMaxEntries: 0,
-  connectTimeoutMS: 10000,
-  // Give up initial connection after 10 seconds
-  socketTimeoutMS: 45000,
-  // Close sockets after 45 seconds of inactivity
-  family: 4 // Use IPv4, skip trying IPv6
-
-};
-exports.default = _default;
+"use strict";var cov_u1iabid56=function(){var a=new Function("return this")(),b="__coverage__",c=a[b]||(a[b]={});return c["/home/wasif/projects/fastify-mongoose-connection/src/option/defaults.js"]&&c["/home/wasif/projects/fastify-mongoose-connection/src/option/defaults.js"].hash==="cb770a2261a060e28f168ffb16333f94cd14fb14"?c["/home/wasif/projects/fastify-mongoose-connection/src/option/defaults.js"]:c["/home/wasif/projects/fastify-mongoose-connection/src/option/defaults.js"]={path:"/home/wasif/projects/fastify-mongoose-connection/src/option/defaults.js",statementMap:{},fnMap:{},branchMap:{},s:{},f:{},b:{},_coverageSchema:"43e27e138ebf9cfc5966b082cf9a028302ed4184",hash:"cb770a2261a060e28f168ffb16333f94cd14fb14"}}();Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;var _default={useNewUrlParser:!0,useCreateIndex:!0,useFindAndModify:!1,autoIndex:!1,// Don't build indexes
+reconnectTries:Number.MAX_VALUE,// Never stop trying to reconnect
+reconnectInterval:500,// Reconnect every 500ms
+poolSize:10,// Maintain up to 10 socket connections
+// If not connected, return errors immediately rather than waiting for reconnect
+bufferMaxEntries:0,connectTimeoutMS:1e4,// Give up initial connection after 10 seconds
+socketTimeoutMS:45e3,// Close sockets after 45 seconds of inactivity
+family:4// Use IPv4, skip trying IPv6
+};exports.default=_default;
 //# sourceMappingURL=defaults.js.map
